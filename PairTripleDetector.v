4869f32fc424fb2e73503893600598f6cb5691bf
jk2997 <jk2997@cornell.edu>
2024-09-06 14:19:37 -0400

    finished implementation

--- PairTripleDetector.v
+++ PairTripleDetector.v
@@ -4,16 +4,24 @@
 
 module PairTripleDetector
 (
-  input  wire in0,
-  input  wire in1,
-  input  wire in2,
+  input wire in0,
+  input wire in1,
+  input wire in2,
   output wire out
 );
+  wire w;
+  wire y;
+  wire x;
+
+  or (w, in0, in1);
+  and (y, w, in2);
+  and (x, in0, in1);
+  or (out, y, x);
+endmodule
 
   // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   // Discussion Section Task
   // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   // Implement a pair/triple detector using explicit gate-level modeling.
 
-endmodule
 
